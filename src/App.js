5d84012c471b17c84cccd51b09f6f61559255007
diyatank2004 <154535295+diyatank2004@users.noreply.github.com>
2024-12-07 20:11:04 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React,{ useState } from 'react';
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  // Link
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+// } from "react-router-dom";
 
 function App() {
   const[mode,setMode]=useState('light');
@@ -41,16 +40,17 @@ function App() {
   }
   return (
     <>
-    <Router>
+    {/* <Router> */}
      <Navbar title="TextUtils" aboutText="About_TextUtils" mode={mode} toggleMode={toggleMode}/>
      <Alert alert={alert}/>
     <div className="container my-3">
-    <Routes>
+    <TextForm showAlert={showAlert} heading="Enter your text below to analyze................." mode={mode}/>
+    {/* <Routes>
     <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter your text below to analyze................." mode={mode}/>} />
     <Route exact path="/about" element={<About mode={mode} toggleMode={toggleMode}/>} />
-        </Routes>
+        </Routes> */}
         </div>
-        </Router>
+        {/* </Router> */}
     </>
   );
 }
